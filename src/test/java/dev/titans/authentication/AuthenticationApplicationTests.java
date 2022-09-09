2dcc8509a31da793b0e0b1c55d7ce48e8215dcae
Myles0911 <mdwalker1@aggies.ncat.edu>
2022-09-09 17:17:16 -0400

    Working on attaching message to login

--- src/test/java/dev/titans/authentication/AuthenticationApplicationTests.java
+++ src/test/java/dev/titans/authentication/AuthenticationApplicationTests.java
@@ -1,18 +1,29 @@
 package dev.titans.authentication;
 
+import dev.titans.dtos.LoginCredentials;
 import dev.titans.entities.User;
 import dev.titans.repos.UserRepo;
+import dev.titans.services.LoginService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jms.core.JmsTemplate;
 
 import java.util.List;
 
 @SpringBootTest
 class AuthenticationApplicationTests {
+	@Autowired
+	LoginService loginService;
+
+		@Autowired
+	JmsTemplate jmsTemplate;
 
 	@Test
 	void contextLoads() {
+		String message = "The Login was performed at 10:00am, Batman";
+		jmsTemplate.convertAndSend("titan-important-events", message);
+
 	}
 
 }
